077d0f1ea2b1c0b98a3ce2b9fd31c2721c37b455
pokerk <1551951508@qq.com>
2018-04-18 21:54:12 +0800

    3

--- urlencode.py
+++ urlencode.py
@@ -2,7 +2,8 @@ from urllib import request
 from urllib import parse
 params = {'name':'张三','age':18,'greet':'hellow world'}
 result = parse.urlencode(params)
-# print(result)
+# encode
+print(result)
 
 # url = 'https://www.baidu.com/s?wd=刘德华'
 url  = 'http://www.baidu.com/s'
@@ -10,5 +11,7 @@ param = {'wd':'张学友'}
 qs  = parse.urlencode(param)
 url = url + '?' + qs
 resp = request.urlopen(url)
-# print(resp.read())
+#content
+print(resp.read())
+#decode
 print(parse.parse_qs(result))
